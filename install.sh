f720a4d9e03e47748f01d337de1042a7287732e3
Eddie Cho <eunseocho@gmail.com>
2023-09-04 10:43:25 -0700

    add basic install script for bootstrapping

---
+++ install.sh
@@ -0,0 +1,11 @@
+# install nix
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+
+# install home-manager
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+nix-shell '<home-manager>' -A install
+
+# setup zsh
+which zsh | sudo tee -A /etc/shells
+chsh -s $(which zsh)
