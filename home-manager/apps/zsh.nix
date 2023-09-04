61ee0c17c15c950607395c9775da522331788307
Eddie Cho <eunseocho@gmail.com>
2023-09-04 12:37:58 -0700

    zsh autocomplete

--- home-manager/apps/zsh.nix
+++ home-manager/apps/zsh.nix
@@ -13,6 +13,7 @@
 
       plugins = [
         "git"
+        "marlonrichert/zsh-autocomplete"
       ];
     };
   };
--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix 
 # install home-manager
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
+# this should also install the home-manager config and set everything up
 nix-shell '<home-manager>' -A install
 
 # setup zsh
