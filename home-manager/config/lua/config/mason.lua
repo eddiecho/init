e15d132b69f57df53faf8fa342c37102184177fd
Eddie Cho <eunseocho@gmail.com>
2025-07-22 08:14:16 +0000

    ugly hack ig

--- home-manager/config/lua/config/mason.lua
+++ home-manager/config/lua/config/mason.lua
@@ -1,13 +1,14 @@
-local idx = 1
 local ensure_installed = {}
+local idx = 1
+
 if vim.fn.executable("go") == 1 then
   ensure_installed[idx] = "gopls"
-  idx += 1
+  idx = idx + 1
 end
 
 if vim.fn.executable("clang") == 1 then
   ensure_installed[idx] = "clangd"
-  idx += 1
+  idx = idx + 1
 end
 
 require("mason-lspconfig").setup{
