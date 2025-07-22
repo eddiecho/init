4b9a07b5fe8551023c070f289053b91be3db7b38
Eddie Cho <eunseocho@gmail.com>
2025-07-22 08:09:13 +0000

    hmm

--- home-manager/config/lua/config/mason.lua
+++ home-manager/config/lua/config/mason.lua
@@ -1,8 +1,16 @@
+local idx = 1
+local ensure_installed = {}
+if vim.fn.executable("go") == 1 then
+  ensure_installed[idx] = "gopls"
+  idx += 1
+end
+
+if vim.fn.executable("clang") == 1 then
+  ensure_installed[idx] = "clangd"
+  idx += 1
+end
+
 require("mason-lspconfig").setup{
-  ensure_installed = {
-    "stylua",
-    "clangd",
-    "gopls",
-  },
+  ensure_installed = ensure_installed,
   automatic_enable = false
 }
