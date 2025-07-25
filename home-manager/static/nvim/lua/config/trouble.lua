ca904b153fd5351ccee45adaccb99081c794ec67
Eddie Cho <eunseocho@gmail.com>
2025-07-25 03:18:17 +0000

    trouble works

--- home-manager/static/nvim/lua/config/trouble.lua
+++ home-manager/static/nvim/lua/config/trouble.lua
@@ -1,34 +1,24 @@
 local t = require("trouble")
 
-    keys = {
-      {
-        "<leader>xx",
-        "<cmd>Trouble diagnostics toggle<cr>",
-        desc = "Diagnostics (Trouble)",
+t.setup({
+  modes = {
+    diagnostics = {
+      auto_open = false,
+      auto_close = true,
+      win = {
+        position = "right",
       },
-      {
-        "<leader>xX",
-        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-        desc = "Buffer Diagnostics (Trouble)",
-      },
-      {
-        "<leader>cs",
-        "<cmd>Trouble symbols toggle focus=false<cr>",
-        desc = "Symbols (Trouble)",
-      },
-      {
-        "<leader>cl",
-        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-        desc = "LSP Definitions / references / ... (Trouble)",
-      },
-      {
-        "<leader>xL",
-        "<cmd>Trouble loclist toggle<cr>",
-        desc = "Location List (Trouble)",
-      },
-      {
-        "<leader>xQ",
-        "<cmd>Trouble qflist toggle<cr>",
-        desc = "Quickfix List (Trouble)",
-      },
-    }
+    },
+  },
+})
+
+local function opts(desc)
+  return {
+    desc = "trouble: " .. desc,
+    noremap = true,
+    silent = true,
+    nowait = true,
+  }
+end
+
+vim.keymap.set("n", "<Leader>xx", function() t.toggle({ mode = "diagnostics", filter = { buf = 0 } }) end, opts("toggle"))
