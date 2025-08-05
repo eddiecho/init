33f7f9bece5fd2ca80cf209ab9d75574e3144151
Eddie Cho <eunseocho@gmail.com>
2025-08-05 06:43:09 +0000

    holy fuck it works

--- home-manager/tools/loadkey/package.nix
+++ home-manager/tools/loadkey/package.nix
@@ -1,7 +1,8 @@
 {pkgs, ...}:
 pkgs.writeShellScriptBin "loadkey" ''
-  printf "\nEnter the seed phrase for your SSH key...\n"
+  read -p "Enter the seed phrase for your SSH key... " seed
+  echo $seed
   mkdir -p ~/.ssh/
-  ${pkgs.melt}/bin/melt restore ~/.ssh/id_ed25519
+  ${pkgs.melt}/bin/melt restore --seed "''${seed}" ~/.ssh/id_ed25519asdffasdfsdaasdf
   printf "\n\nContinuing activation.\n\n"
 ''
