ec125c95ba6e42d2a43cdb72a6bb20386d71d8d5
Eddie Cho <eunseocho@gmail.com>
2025-08-11 07:14:16 +0000

    i really should use the lsp commands more

--- static/nvim/lua/lsp.lua
+++ static/nvim/lua/lsp.lua
@@ -14,13 +14,14 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
     vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
     vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "gh", vim.lsp.buf.signature_help, opts)
     vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
     vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
 
     vim.keymap.set("n", "<space>wl", function()
       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, opts)
+
     vim.keymap.set("n", "<space>f", function()
       vim.lsp.buf.format { async = true }
     end, opts)
