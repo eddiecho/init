c7a625d6ddf2f45a95128f273c370c80a80e716b
Eddie Cho <eunseocho@gmail.com>
2025-08-11 21:08:38 -0700

    init darwin

---
+++ modules/darwin/common.nix
@@ -0,0 +1,55 @@
+{
+config,
+  pkgs,
+  lib,
+  ...
+}: let
+  inherit (config.settings) username;
+  cfg = config.darwin.common;
+in {
+  options.darwin.common.enable = lib.mkEnableOption "Enable common";
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      curl
+      fd
+      file
+      git
+      home-manager
+      htop
+      jq
+      neovim
+      ripgrep
+      tldr
+      unzip
+      pkg-config
+    ];
+
+    nix = {
+      extraOptions = ''
+        experimental-features = nix-command flakes
+        warn-dirty = false
+      '';
+
+      gc = {
+        automatic = true;
+        options = "--delete-older-than 7d";
+      };
+
+      settings = {
+        trusted-users = [
+          "root"
+          "@wheel"
+          username
+        ];
+
+        allowed-users = [
+          "@wheel"
+          username
+        ];
+
+        auto-optimise-store = lib.mkIf (!pkgs.stdenv.isDarwin) true;
+      };
+    };
+  };
+}
