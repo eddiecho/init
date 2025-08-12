b161e56b027a4733d6bd3f901ce523cbc0992871
Eddie Cho <eunseocho@gmail.com>
2025-08-12 01:02:05 -0700

    i guess this is deprecated?

--- static/nvim/lua/config/gitsigns.lua
+++ static/nvim/lua/config/gitsigns.lua
@@ -8,11 +8,11 @@ local gs = require "gitsigns"
 
 require "gitsigns".setup {
   signs = {
-    add = { hl = "DiffAdd", text = "▌", numhl = "GitSignsAddNr" },
-    change = { hl = "DiffChange", text = "▌", numhl = "GitSignsChangeNr" },
-    delete = { hl = "DiffDelete", text = "_", numhl = "GitSignsDeleteNr" },
-    topdelete = { hl = "DiffDelete", text = "‾", numhl = "GitSignsDeleteNr" },
-    changedelete = { hl = "DiffChange", text = "~", numhl = "GitSignsChangeNr" }
+    add = { text = "▌", },
+    change = { text = "▌", },
+    delete = { text = "_", },
+    topdelete = { text = "‾", },
+    changedelete = { text = "~", }
   },
   numhl = false,
   watch_gitdir = {
