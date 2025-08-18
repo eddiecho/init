89b281c8f8e711e516ff70fe7a1833ed9ad64cf0
Eddie Cho <eunseocho@gmail.com>
2025-08-18 13:52:00 -0700

    update treesitter branch

--- static/nvim/lua/plugins/treesitter.lua
+++ static/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     lazy = false,
+    branch = "main",
     build = function(plug)
       pcall(require("nvim-treesitter.install").update { with_sync = true })
     end,
