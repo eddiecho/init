14c166cedb795948196d88746b762d33ac27a692
Eddie Cho <eunseocho@gmail.com>
2025-09-22 23:24:38 -0700

    well it builds

--- hosts/x86_64-linux/framework/default.nix
+++ hosts/x86_64-linux/framework/default.nix
@@ -3,7 +3,7 @@ let
   vals = {
     username = "eddie";
     fullName = "Eddie Cho";
-    stateVersion = "24.11";
+    stateVersion = "25.05";
   };
 in {
   home-manager.users.${vals.username} = {
@@ -31,5 +31,27 @@ in {
     loader.efi.canTouchEfiVariables = true;
   };
 
+  users.mutableUsers = true;
   system.stateVersion = vals.stateVersion;
+
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/18ed4b766-5ac2-4c1e-baef-eca238f1c0e7";
+      fsType = "ext4";
+    };
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/E204-4398";
+      fsType = "vfat";
+      options = [ "fmast=0077" "dmask=0077" ];
+    };
+
+  swapDevices = [ ];
+
+  networking.useDHCP = true;
+  nixpkgs.hostPlatform = "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = true;
 }
