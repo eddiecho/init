857ec96aaedbeb558e99314b2ad140498c0a709f
Eddie Cho <eunseocho@gmail.com>
2025-09-24 05:07:51 +0000

    fix loadkey key name

--- tools/loadkey/package.nix
+++ tools/loadkey/package.nix
@@ -3,6 +3,6 @@ pkgs.writeShellScriptBin "loadkey" ''
   read -p "Enter the seed phrase for your SSH key... " seed
   echo $seed
   mkdir -p ~/.ssh/
-  ${pkgs.melt}/bin/melt restore --seed "''${seed}" ~/.ssh/id_ed25519asdffasdfsdaasdf
+  ${pkgs.melt}/bin/melt restore --seed "''${seed}" ~/.ssh/id_ed25519
   printf "\n\nContinuing activation.\n\n"
 ''
