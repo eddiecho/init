795f4f0fa47fb779d0167ac2f2d452f9219a82ee
Eddie Cho <eunseocho@gmail.com>
2025-09-29 17:33:06 +0000

    nix lsp is annoying

--- static/nvim/lua/lspconfigd/nix.lua
+++ static/nvim/lua/lspconfigd/nix.lua
@@ -1,3 +1,14 @@
 if vim.fn.executable("nix") == 1 then
   vim.lsp.enable("nil_ls")
+  vim.lsp.config("nil_ls", {
+    settings = {
+      ['nil'] = {
+        nix = {
+          flake = {
+            autoArchive = true,
+          },
+        },
+      },
+    },
+  })
 end
