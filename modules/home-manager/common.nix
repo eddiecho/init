6dae5070e7f0408ec19c2a22b0f4cbeea16acff7
Eddie Cho <eunseocho@gmail.com>
2025-10-01 06:58:58 +0000

    hmm

--- modules/home-manager/common.nix
+++ modules/home-manager/common.nix
@@ -19,9 +19,8 @@ in {
 
     home.packages = with pkgs; [
       clang_20
-      cargo # way too many nix things depend on this not to have zzz
+      # cargo # way too many nix things depend on this not to have zzz
       gnumake
-      tree-sitter
     ];
 
     modules.apps = {
