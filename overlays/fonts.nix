bd9ef012d38bc452a120f15264278f30ff3044b4
Eddie Cho <eunseocho@gmail.com>
2025-10-01 01:56:12 +0000

    test

---
+++ overlays/fonts.nix
@@ -0,0 +1,12 @@
+(final: prev: {
+  sfmono = prev.stdenvNoCC.mkDerivation rec {
+    pname = "sfmono";
+    version = "dev";
+    src = inputs.sfmono;
+    dontConfigure = true;
+    installPhase = ''
+      mkdir -p $out/share/fonts/opentype
+      cp -R $src/*.otf $out/share/fonts/opentype
+    '';
+  };
+})
