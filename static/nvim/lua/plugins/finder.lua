fbf5a19991abddc65795e930cbfb4fe3ca95cc83
Eddie Cho <eunseocho@gmail.com>
2026-03-19 02:12:23 -0700

    Revert "oh he has prebuilt binaries"
    This reverts commit fd2d5eb3eeb655f2f07c5778dfb056d9383986c9.

--- static/nvim/lua/plugins/finder.lua
+++ static/nvim/lua/plugins/finder.lua
@@ -1,8 +1,16 @@
+-- On Windows Neovide, I think Lazy holds a lock on the directory or something?
+-- I have to close out Neovide and then manually recompile to get it working
+local build_func = function()
+	if vim.fn.executable("nix") == 1 then
+		return "nix run .#release"
+	end
+
+	return "cargo build --release"
+end
+
 return {
 	"dmtrKovalenko/fff.nvim",
-	build = function()
-    require("fff.download").download_or_build_binary()
-  end,
+	build = build_func(),
 	config = function()
 		require("config.finder")
 	end,
--- static/nvim/plugin/finder.lua
+++ static/nvim/plugin/finder.lua
@@ -23,8 +23,7 @@ if vim.pack ~= nil then
 			local plugin_name = "fff.nvim"
 
 			if name == plugin_name and (kind == "install" or kind == "update") then
-        require("fff.download").download_or_build_binary()
-				-- utils.build_plugin_with_logs(plugin_name, ev.data.path, build_cmd())
+				utils.build_plugin_with_logs(plugin_name, ev.data.path, build_cmd())
 			end
 		end,
 	})
