4561e13d5e85f58270bdc38bd2184ec2de5a9894
Eddie Cho <eunseocho@gmail.com>
2026-04-02 01:08:58 -0700

    revert back to stable nixpkgs but add the FUCKING overlay for nvim

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*";
 
     determinate = {
       url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
