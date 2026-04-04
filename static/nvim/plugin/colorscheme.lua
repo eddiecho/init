de1251bad4f89d29a1b7980905a700ddc9bad903
Eddie Cho <eunseocho@gmail.com>
2026-04-04 02:09:27 -0700

    im illiterate

--- static/nvim/plugin/colorscheme.lua
+++ static/nvim/plugin/colorscheme.lua
@@ -11,42 +11,38 @@ if vim.pack ~= nil then
 	local transparent = utils.is_linux()
 
 	require("catppuccin").setup({
-		name = "catppuccin",
-		lazy = false,
-		priority = 1000,
-		opts = {
-			flavour = "mocha",
-			transparent_background = transparent,
-			float = {
-				transparent = transparent,
+		flavour = "mocha",
+		transparent_background = transparent,
+		float = {
+			solid = false,
+			transparent = transparent,
+		},
+		integrations = {
+			cmp = true,
+			gitsigns = true,
+			nvimtree = true,
+			treesitter = true,
+			fidget = true,
+			indent_blankline = {
+				enabled = true,
+				colored_indent_levels = false,
 			},
-			integrations = {
-				cmp = true,
-				gitsigns = true,
-				nvimtree = true,
-				treesitter = true,
-				fidget = true,
-				indent_blankline = {
-					enabled = true,
-					colored_indent_levels = false,
+			native_lsp = {
+				enabled = true,
+				virtual_text = {
+					errors = { "italic" },
+					hints = { "italic" },
+					warnings = { "italic" },
+					information = { "italic" },
 				},
-				native_lsp = {
-					enabled = true,
-					virtual_text = {
-						errors = { "italic" },
-						hints = { "italic" },
-						warnings = { "italic" },
-						information = { "italic" },
-					},
-					underlines = {
-						errors = { "underline" },
-						hints = { "underline" },
-						warnings = { "underline" },
-						information = { "underline" },
-					},
-					inlay_hints = {
-						background = true,
-					},
+				underlines = {
+					errors = { "underline" },
+					hints = { "underline" },
+					warnings = { "underline" },
+					information = { "underline" },
+				},
+				inlay_hints = {
+					background = true,
 				},
 			},
 		},
