48114f3f09928e60c98a222589915471ec6f5bba
Eddie Cho <eddie.cho@rubrik.com>
2026-05-20 12:28:40 -0700

    move more to lua

---
+++ static/hypr/parts/variables.lua
@@ -0,0 +1,7 @@
+-- Globals (visible to later parts loaded via dofile in the same Lua state)
+mainMod = "SUPER"
+terminal = "ghostty"
+webBrowser = "firefox"
+menu = "vicinae toggle"
+-- fileManager = "nautilus"
+-- notes = "obsidian"
