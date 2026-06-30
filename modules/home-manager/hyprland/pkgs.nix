4e934a5139ebb15214d508e321c8a698489c3a7e
Eddie Cho <eunseocho@gmail.com>
2026-06-30 00:37:52 -0700

    bweh

--- modules/home-manager/hyprland/pkgs.nix
+++ modules/home-manager/hyprland/pkgs.nix
@@ -16,7 +16,6 @@ in {
       hyprlock # lock screen
       hypridle # idle daemon (whatever that means?)
       tofi # popup program switcher
-      # hyprpaper # i don't know the difference between this and awww
       linux-wallpaperengine
       swaynotificationcenter # swaync
     ];
